628228791540f842babe2e3b8201304875e0b3e5
Les Boyd <lboyd@student.sdccd.edu>
2024-11-10 22:28:31 -0800

    Hour 11 with loops
    added loops in start to instantiate 10 lamps

--- lboyd_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ lboyd_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -8,7 +8,13 @@ public class PrefabGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i=0; i<10; i++)
+        {
+            //Instantiate(prefab);
+            //Instantiate(prefab, transform.position, transform.rotation);
+            Vector3 vector = new Vector3(1f*5*i, 1f, 1f);
+            Instantiate(prefab, vector, transform.rotation);
+        }
     }
 
     // Update is called once per frame
