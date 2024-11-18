a04f0251744380ad75dc4d52532461b1fa9386c7
Les Boyd <lboyd@student.sdccd.edu>
2024-11-18 00:25:17 -0800

    hour 16 particle system
    finished the hour 16 assignment

---
+++ lboyd_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,17 @@
+/*
+ * 1) added the particle effect to a sphere to make it look like a comet trail 
+ * 
+ * 2) adjust the emission
+ * 
+ * 3) adjusted the shape so it looks like it's coming out the back of the meteor
+ * 
+ * 4) changed the velocity over lifetime
+ * 
+ * 5) added color over lifetime
+ * 
+ * 6) added color by speed
+ * 
+ * 7) added trails that streak off behind as well
+ * 
+ *  
+ */
---
+++ lboyd_Hour14and16/Assets/UserInterface.cs
@@ -0,0 +1,7 @@
+/*
+ * 1)   Added a different background image to the hour scene
+ * 2)   Added new titles and text and a button that takes you to another scene
+ * 3)   The new scene has a back button that takes you back to the previous scene
+ * 4)   There are two new buttons that enable music and disable it
+ *  
+ */
