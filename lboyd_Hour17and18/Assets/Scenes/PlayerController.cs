0c82972dab824da2e38a6f2c5776bdfcc39f9255
Les Boyd <lboyd@student.sdccd.edu>
2024-11-24 21:40:48 -0800

    Hour 17 and 18
    The Hour 17 work from the book and the Canvas Module. Hour 18 Ethan animation from the book.

---
+++ lboyd_Hour17and18/Assets/Scenes/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            animator.SetTrigger("Color");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            animator.SetTrigger("Scale");
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            animator.SetTrigger("Spin");
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetTrigger("Jump");
+        }
+    }
+}
